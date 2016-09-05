f8b1406ee53c36f457244c0efa46c3d6da15978c
jmeier07 <jmeie@DESKTOP-BJO0TA9>
2016-09-05 16:41:12 -0400

    added some additional ng-repeat functionality

--- AngularMessing/AngularMessing/Scripts/QueryApp/QueryApp.js
+++ AngularMessing/AngularMessing/Scripts/QueryApp/QueryApp.js
@@ -1,5 +1,11 @@
 ﻿var myApp = angular.module('myApp',[]);
 
 myApp.controller('GreetingController', ['$scope', function($scope) {
-  $scope.greeting = 'Hola!';
+    $scope.greeting = 'Hola!';
+    $scope.queryCriteria = ["firstName","lastName"];
+    $scope.results = [
+    { "firstName": "John", "lastName": "Doe" },
+    { "firstName": "Anna", "lastName": "Smith" },
+    { "firstName": "Peter", "lastName": "Jones" }
+    ];
 }]);
